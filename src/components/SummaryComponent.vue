399ae07920dbab684bbba8ca737a23a5f7706bbc
ZD3NY <45035855+ZD3NY@users.noreply.github.com>
2024-06-30 17:45:15 +0200

    Update SummaryComponent.vue

--- src/components/SummaryComponent.vue
+++ src/components/SummaryComponent.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="bg-gray-100 rounded-lg p-4">
+    <div class="bg-gray-100 rounded-lg p-4 sm:text-sm md:text-xs">
       <div class="flex items-center justify-start mb-7">
-        <img src="@/assets/notes-logo.svg" alt="Notes Logo" class="h-8 w-8 mr-2">
+        <img src="@/assets/notes-logo.svg" alt="Notes Logo" class="h-6 w-6 mr-2 ms:h-8 sm:w-8 md:h-6 md:w-6"">
         <h3 class="text-lg font-bold mb-2">Celkový souhrn</h3>
       </div>
       <div class="flex space-x-4 mb-4">
         <div v-for="(summary, index) in filteredSummaries" :key="index" class="flex items-center space-x-4">
-          <img v-if="index === 0" :src="stairsIcon" alt="Stairs icon" class="h-8 w-8" />
-          <img v-else-if="index === 1" :src="doorIcon" alt="Door icon" class="h-8 w-8" />
+          <img v-if="index === 0" :src="stairsIcon" alt="Stairs icon" class="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6" />
+          <img v-else-if="index === 1" :src="doorIcon" alt="Door icon" class="h-6 w-6 sm:h-8 sm:w-8 md:h-6 md:w-6" />
           <div class="ml-2">
             <p class="text-gray-700 font-medium">{{ summary.title }}</p>
             <p class="text-gray-500">{{ summary.value }}</p>
           </div>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="summary.action">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded text-xs sm:py-2 sm:px-4 sm:text-sm md:py-1 md:px-2 md:text-xs lg:py-2 lg:px-4 lg:text-sm w-auto min-w-0" @click="summary.action">
             {{ summary.buttonText }}
           </button>
         </div>
